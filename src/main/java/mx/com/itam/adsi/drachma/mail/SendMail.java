01645ccc1ad2d93a03bf7035210c565e5a8c2984
Moni <adsiitam03@gmail.com>
2018-05-26 22:26:36 -0500

    clase terminada

--- src/main/java/mx/com/itam/adsi/drachma/mail/SendMail.java
+++ src/main/java/mx/com/itam/adsi/drachma/mail/SendMail.java
@@ -13,31 +13,84 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import javax.ws.rs.core.MediaType;
+import org.apache.log4j.Logger; 
 
+/**
+*Clase SendMail
+*
+*Se encarga de enviar un correo del tipo requerido al destinatario deseado.
+**/
 public class SendMail {
+
+	private final static Logger LOG = Logger.getLogger(SendMail.class);
+
+	/**
+	*Correo del destinatario
+	*/
 	private String mail;
-	private String msgBueno = "Hola, este correo es para informarle que el precio de la criptomoneda ha bajado drásticamente."; //mensaje cuando hay que comprar
-	private String msgMalo = "Hola, este correo es para informarle que el precio de la criptomoneda ha subido drásticamente."; //mensaje cuando hay que vender
-	public SendMail(String m) {
+	/**
+	*Tipo de correo a enviar
+	*/
+	private int tipo; //si es 0, aumenta precio, si no, disminuye
+
+	public SendMail(String m, int t) {
 		mail = m;
+		tipo = t;
 	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		SendMail p = new SendMail("adsi.itam.03@gmail.com");
-		p.SendSimpleMessage();
+		SendMail p = new SendMail("adsi.itam.03@gmail.com",1);
+		p.enviaMail();
 
 	}
 
-	public ClientResponse SendSimpleMessage() {
+	/**
+	*Envía correo al destinatario
+	*@param cambio tipo de cambio en la cripto moneda
+	*@return respuesta del servidor de correo
+	*/
+	public ClientResponse SendSimpleMessage(String cambio) {
+	    LOG.info("Enviando correo a destinatario");
 	    Client client = Client.create();
 	    client.addFilter(new HTTPBasicAuthFilter("api", "key-88d4a0b737fbec0f8f5f70a738dff845"));
 	    WebResource webResource = client.resource("https://api.mailgun.net/v3/sandbox01f157d5443b43a5a07afc898d461c7e.mailgun.org/messages");
 	    MultivaluedMapImpl formData = new MultivaluedMapImpl();
+	    //Aquí se ponen los datos del correo
 	    formData.add("from", "Drachma  <postmaster@sandbox01f157d5443b43a5a07afc898d461c7e.mailgun.org>");
 	    formData.add("to", mail);
-	    formData.add("subject", "Helloooooow");
-	    formData.add("text", "No es para presumir, pero creo que tu novia es genial!");
+	    formData.add("subject", "Cambio en el precio de la criptomoneda");
+	    //Aquí se escribe el cuerpo del correo
+	    formData.add("text", "Hola, este correo es para informarle que el precio de la criptomoneda ha "+cambio+" notoriamente.");
+	    formData.add("text", "Saludos");
+	    formData.add("text", "Drachma");
 	    return webResource.type(MediaType.APPLICATION_FORM_URLENCODED).
 	                                        post(ClientResponse.class, formData);
 	}
+
+	/**
+	*Llama a SendSimpleMessage y guarda información del correo enviado
+	*@return true si el mensaje se envió correctamente, false en caso contrario
+	*/
+	public boolean enviaMail(){
+		LOG.info("Destinatario: "+mail);
+		String tipoM = "Tipo de correo: ";
+		ClientResponse r = null;
+		if(tipo == 0){
+			LOG.info(tipoM+"disminuye precio");
+			r = SendSimpleMessage("disminuido");
+		}else{
+			LOG.info(tipoM+"aumenta precio");
+			r = SendSimpleMessage("aumentado");
+		}
+		
+		if(r.getStatus() == 200){
+			LOG.info("Correo enviado correctamente!");
+			return true;
+		}else{
+			LOG.info("No se ha logrado enviar el correo");
+			return false;
+		}
+	}
+		
 }
